230812287fbe1f90d84cf93e1af9c2346af679b2
K7 <32661918+anjooficial@users.noreply.github.com>
2022-07-26 04:15:18 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 clear
+soft="python bc screen at nano unzip lsof netstat net-tools dos2unix nload jq python3 python-pip"
+   for i in $soft; do
+    leng="${#i}"
+    puntos=$((21 - $leng))
+    pts="."
+    for ((a = 0; a < $puntos; a++)); do
+      pts+="."
+    done
+    msg -nazu "    INSTALANDO $i$(msg -ama "$pts")"
+    if apt install $i -y &>/dev/null; then
+      msg -verd " INSTALADO"
+    else
+      msg -verm2 " ERRO"
+      sleep 2
+      tput cuu1 && tput dl1
+      print_center -ama "APLICANDO FIX A $i"
+      dpkg --configure -a &>/dev/null
+      sleep 2
+      tput cuu1 && tput dl1
+
+      msg -nazu "    INSTALANDO $i$(msg -ama "$pts")"
+      if apt install $i -y &>/dev/null; then
+        msg -verd " INSTALADO"
+      else
+        msg -verm2 " ERRO"
+      fi
+    fi
+  done
