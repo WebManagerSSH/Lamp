6aebe57a57028ca9a76d5d0ba627da1da17318d8
K7 <32661918+anjooficial@users.noreply.github.com>
2022-07-26 04:18:39 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 clear
-soft="python bc screen at nano unzip lsof netstat net-tools dos2unix nload jq python3 python-pip"
+function msg {
+  BRAN='\033[1;37m' && RED='\e[31m' && GREEN='\e[32m' && YELLOW='\e[33m'
+  BLUE='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && BLACK='\e[1m' && SEMCOR='\e[0m'
+  case $1 in
+  -ne) cor="${RED}${BLACK}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -ama) cor="${YELLOW}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -verm) cor="${YELLOW}${BLACK}[!] ${RED}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -azu) cor="${MAG}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -verd) cor="${GREEN}${BLACK}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -bra) cor="${RED}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -nazu) cor="${COLOR[6]}${BLACK}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -gri) cor="\e[5m\033[1;100m" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  "-bar2" | "-bar") cor="${RED}————————————————————————————————————————————————————" && echo -e "${SEMCOR}${cor}${SEMCOR}" ;;
+  esac
+}
+function dependencias {
+  soft="python bc screen at nano unzip lsof netstat net-tools dos2unix nload jq python3 python-pip"
    for i in $soft; do
     leng="${#i}"
     puntos=$((21 - $leng))
@@ -28,3 +44,26 @@ soft="python bc screen at nano unzip lsof netstat net-tools dos2unix nload jq py
       fi
     fi
   done
+}
+
+function install_continue {
+  os_system
+  msg -bar
+  echo -e "      \e[5m\033[1;100m   INSTALANDO PACOTES PARA O SCRIPT   \033[1;37m"
+  msg -bar
+  print_center -ama "$distro $vercion"
+  print_center -verd "INSTALANDO DEPENDÊNCIAS"
+  msg -bar3
+  dependencias
+  msg -bar3
+  print_center -azu "Removendo pacotes obsoletos"
+  apt autoremove -y &>/dev/null
+  sleep 2
+  tput cuu1 && tput dl1
+  msg -bar
+  print_center -ama "Se algumas das dependências falharem!!!\nQuando terminar, você pode tentar instalar\no mesmo manualmente usando o seguinte comando\napt install nome_do_pacote"
+  msg -bar
+  read -t 60 -n 1 -rsp $'\033[1;39m       << Pressione enter para continuar >>\n'
+}
+
+install_continue
